66dc4839bd0bb335846b234c35e1167e29936d64
Jorge Ordovás <jorge.ordovas@gmail.com>
2025-03-23 09:11:54 +0100

    Update HM25.h

--- src/contracts/HM25.h
+++ src/contracts/HM25.h
@@ -20,26 +20,8 @@ public:
         uint64 lastMilestone;
     };
 
-    struct GetBudget_input {};
-    struct GetBudget_output
-    {
-        uint64 budget;
-    };
-
 private:
     uint64 lastMilestone;
-    uint64 budget;
-
-    /**
-    Send back the invocation amount
-    
-    PUBLIC_PROCEDURE(PayBudget)
-        if (qpi.invocationReward() > 0)
-        {
-            state.budget = state.budget - qpi.invocationReward;
-            qpi.transfer(qpi.invocator(), qpi.invocationReward());
-        }
-    */
     _
 
     _
@@ -49,22 +31,15 @@ private:
 
     PUBLIC_FUNCTION(GetMilestone)
         output.lastMilestone = state.lastMilestone;
-
-    PUBLIC_FUNCTION(GetBudget)
-        output.budget = state.budget;
     _
 
     REGISTER_USER_FUNCTIONS_AND_PROCEDURES
 
-        /* REGISTER_USER_PROCEDURE(PayBudget, 1); */
-
         REGISTER_USER_FUNCTION(SetMilestone, 1);
         REGISTER_USER_FUNCTION(GetMilestone, 2);
-        REGISTER_USER_FUNCTION(GetBudget, 3);
     _
 
     INITIALIZE
         state.lastMilestone = 0;
-        state.budget = 1000;
     _
 };
